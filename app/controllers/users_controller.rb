6a0193df4e5ff60761b127b234b146b264e35cbe
mikeporter <porter.michael.james@gmail.com>
2017-02-15 19:22:43 -0800

    2_15 freeze to finish old commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update, :destroy]
+
 	def index
 		@users = User.all
 	end
+
 	def show
 		@user = User.find(params[:id])
     @departments = @user.departments
@@ -15,16 +17,16 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
-  #def create
-  #  user = User.find_by(email: params[:session][:email].downcase)
-  #  if user && user.authenticate(params[:session][:password])
-  #    log_in user
-  #    redirect_back_or user
-  #  else
-  #    flash.now[:danger] = 'Invalid email/password combination'
-  #    render 'new'
-  #  end
-  #end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_back_or user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 	
   def edit
     @user = User.find(params[:id])
